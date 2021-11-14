19da77611f93924bd10a83e33b59053b7fbdf749
DoodleHubz <chaosauce93@gmail.com>
2021-11-14 15:14:17 -0500

    Add - global styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 
+// Styles
+import { GlobalStyle } from './GlobalStyles';
+
 const App: React.FC = () => (
   <Router>
     <Header />
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
+    <GlobalStyle />
   </Router>
 );
 
