dfc9d7fea7e8496df1065e7dcef5363aeb1da651
DoodleHubz <chaosauce93@gmail.com>
2021-11-14 14:41:24 -0500

    Set up Header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Components
+import Header from './components/Header';
 import Home from './components/Home';
 
 const App: React.FC = () => (
   <Router>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Styles
+import { Wrapper } from './Header.styles';
+
+const Header: React.FC = () => (
+  <div>This is the header</div>
+);
+
+export default Wrapper;
