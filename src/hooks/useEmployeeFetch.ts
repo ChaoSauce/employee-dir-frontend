460dfc6b7debcd57a3a0118f996c46ce6d9761f9
DoodleHubz <chaosauce93@gmail.com>
2021-11-14 20:11:40 -0500

    Update - useEmployeeFetch hook

---
+++ src/hooks/useEmployeeFetch.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import API, { Employee } from '../api';
+
+export const useEmployeeFetch = (employeeId: string) => {
+  const [state, setState] = useState<Employee>({} as Employee);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchEmployee = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+
+        const employee = await API.fetchEmployee(employeeId);
+
+        setState({ ...employee });
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    };
+
+    fetchEmployee();
+  }, [employeeId]);
+
+  return { state, loading, error };
+};
