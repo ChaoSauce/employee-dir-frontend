840cfec46f5d8e167753c898a95e167a6d63c1b0
DoodleHubz <chaosauce93@gmail.com>
2021-11-16 18:06:11 -0500

    Set up NewEmployee component

---
+++ src/components/NewEmployee/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Styles
+import { Wrapper } from './NewEmployee.styles';
+
+const NewEmployee: React.FC = () => (
+  <Wrapper>
+    <div>This is a form</div>
+  </Wrapper>
+);
+
+export default NewEmployee;
